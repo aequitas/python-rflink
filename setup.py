a304d93f42f6eb0e436ed00a5db307adafd52085
Johan Bloemberg <github@ijohan.nl>
2016-11-17 23:30:59 +0100

    Bump.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='rflink',
 
-    version='0.0.1',
+    version='0.0.2',
 
     description=__doc__,
     long_description=long_description,
