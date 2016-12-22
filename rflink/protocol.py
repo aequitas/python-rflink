4606e5cdec00476decbbb48c5dd393dce4dcd16c
Johan Bloemberg <github@ijohan.nl>
2016-12-22 20:57:32 +0100

    Propagate event callback.

--- rflink/protocol.py
+++ rflink/protocol.py
@@ -283,10 +283,12 @@ def create_rflink_connection(*args, **kwargs):
     # use default protocol if not specified
     rflink_protocol = kwargs.pop('protocol', EventHandling)
     packet_callback = kwargs.pop('packet_callback', None)
+    event_callback = kwargs.pop('event_callback', None)
     protocol = partial(
         rflink_protocol,
         loop,
         packet_callback=packet_callback,
+        event_callback=event_callback,
         ignore=kwargs.pop('ignore')
     )
 
